09479f1c15baffaeca3e2e1a3ee950313afc3133
yyogaagung <gcyogaagung123@gmail.com>
2022-12-07 11:08:34 +0700

    initial commit

---
+++ dist/js/script.js
@@ -0,0 +1,20 @@
+        // HAMBURGER
+        const hamburger = document.querySelector('#hamburger');
+        const navMenu = document.querySelector('#nav_menu');
+
+        hamburger.addEventListener('click', function(){
+            hamburger.classList.toggle('hamburger-active');
+            navMenu.classList.toggle('hidden');
+        } )
+
+        // Navbar fixed
+        window.onscroll = function(){
+        const header = document.querySelector('header');
+
+        const fixedNav = header.offsetTop;
+        if(window.pageYOffset > fixedNav){
+            header.classList.add('navbar-fixed');
+        }else{
+            header.classList.remove('navbar-fixed');
+        }
+        }
